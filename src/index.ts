da6a390c0db8a23c8d913413e8223218c744c751
kianiehsan66 <kiani.ehsan66@gmail.com>
2024-11-22 15:24:34 +0000

    qwsx9

--- src/index.ts
+++ src/index.ts
@@ -9,21 +9,7 @@ app.use(express.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
   const currentTime = new Date().toLocaleString();
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Express HTML Response</title>
-      </head>
-      <body>
-        <h1>Hello, World!</h1>
-        <p>This is a simple HTML response from an Express app.</p>
-         <p>Current Server Time: ${currentTime}</p>
-      </body>
-    </html>
-  `);
+  res.status(501);
 });
 
 app.listen(port, () => {
