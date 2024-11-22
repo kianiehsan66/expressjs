bc5075f33a616ae99bceb3504b44b9ce1a22b56c
kianiehsan66 <kiani.ehsan66@gmail.com>
2024-11-22 15:08:40 +0000

    qwsx7

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  res.set("Cache-Control", "s-maxage=60");
+  res.set("set-cookie", "blah=60");
   const currentTime = new Date().toLocaleString();
   res.send(`
     <!DOCTYPE html>
